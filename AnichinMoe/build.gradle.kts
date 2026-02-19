62ae2039742824ef474d3fcf1763ca2cf9a3bb53
FZRDHN <153108555+kakikakiseribu@users.noreply.github.com>
2026-02-19 21:50:06 +0800

    Build 6b1cc310abd495ae22f9564d685ce6f4d85cb2e5

---
+++ AnichinMoe/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    description = "Anichin Moe"
+    language = "id"
+    authors = listOf("Fauzi Ridhuan")
+    status = 1
+    tvTypes = listOf("AnimeMovie", "Anime", "Cartoon")
+    isCrossPlatform = true
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "AsianDrama",
+    )
+}
